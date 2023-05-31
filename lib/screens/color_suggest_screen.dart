8e8204bc52b6b23b562fa8ed27e8243816562392
foundaway98 <foundaway98@naver.com>
2023-06-01 01:17:14 +0900

    colorpicker 추가 및 색 데이터 저장

--- lib/screens/color_suggest_screen.dart
+++ lib/screens/color_suggest_screen.dart
@@ -1,20 +1,44 @@
+import 'package:colornames/colornames.dart';
+import 'package:cyclop/cyclop.dart';
 import 'package:flutter/material.dart';
 
 class ColorSuggestScreen extends StatefulWidget {
   const ColorSuggestScreen({super.key, required this.color});
 
   final String color;
+
   @override
   State<ColorSuggestScreen> createState() => _ColorSuggestScreenState();
 }
 
 class _ColorSuggestScreenState extends State<ColorSuggestScreen> {
+  List<Color> colorPack = [
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+  ];
+
+  List<bool> colorFlag = [false, false, false, false, false];
+
+  List<String> colorTester = ["test1", "test2", "test3", "test4", "test5"];
   List<String> colorSuggestionList = ["a", "b", "c"];
   List<List<String>> colorSet = [
     ["r", "g", "b"],
     ["r", "y", "b", "a"],
     ["r", "w", "g", "g", "t"],
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.color != "") {
+      colorPack[0] = widget.color.toColor();
+      colorFlag[0] = true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +48,6 @@ class _ColorSuggestScreenState extends State<ColorSuggestScreen> {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(widget.color),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -46,60 +69,99 @@ class _ColorSuggestScreenState extends State<ColorSuggestScreen> {
             ),
             Flexible(
               flex: 1,
-              child: Container(
-                decoration: widget.color == ''
-                    ? const BoxDecoration()
-                    : BoxDecoration(
-                        color: Color(
-                          int.parse(widget.color, radix: 16),
-                        ),
-                      ),
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 65, horizontal: 20),
-                child: const Text(
-                  "Color name",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
+              fit: FlexFit.tight,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  for (int i = 0; i < colorPack.length; i++)
+                    makeColorPickerButton(context, i),
+                ],
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add_box_outlined),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add_box_outlined),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add_box_outlined),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add_box_outlined),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.add_box_outlined),
-                )
-              ],
-            ),
-            makeList(colorSuggestionList)
+            Flexible(
+              flex: 3,
+              fit: FlexFit.tight,
+              child: Container(child: makeList(colorSuggestionList)),
+            )
           ],
         ));
   }
 
+  Padding makeColorPickerButton(BuildContext context, i) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                colorFlag[i] = false;
+                colorPack[i] = Colors.white;
+              });
+            },
+            child: const SizedBox(
+                width: 20,
+                child: Text(
+                  "x",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                )),
+          ),
+          GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    child: ColorPicker(
+                      selectedColor: colorPack[i],
+                      onColorSelected: (value) {
+                        setState(() {
+                          colorFlag[i] = true;
+                          colorPack[i] = value;
+                          print(colorFlag);
+                        });
+                      },
+                      config: const ColorPickerConfig(
+                        enableLibrary: false,
+                        enableEyePicker: false,
+                        enableOpacity: false,
+                      ),
+                      onClose: Navigator.of(context).pop,
+                      onEyeDropper: () {},
+                    ),
+                  );
+                },
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(color: colorPack[i]),
+              width: 50,
+              height: 50,
+              margin: const EdgeInsets.only(top: 10),
+            ),
+          ),
+          SizedBox(
+            width: 50,
+            child: Text(
+              colorPack[i].colorName,
+              style: const TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   ListView makeList(List<String> colorSuggestionList) {
     return ListView.separated(
-        shrinkWrap: true,
+        shrinkWrap: false,
         itemBuilder: (context, index) {
           return Column(
             children: [
